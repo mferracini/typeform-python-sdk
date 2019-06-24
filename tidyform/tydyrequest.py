77911c2ad32d91b0b42e20c557bfadc58ff8fdc3
Mateus Ferracini <mateus.ferracini@CAMPUSM002184.local>
2019-06-24 16:03:19 -0300

    functoins to tidy infos

--- tidyform/tydyrequest.py
+++ tidyform/tydyrequest.py
@@ -5,21 +5,111 @@ import pandas as pd
 import json
 
 def tidylist(formList: dict):
-    df = pd.DataFrame(columns=['id', 'title', 'last_updated_at','settings','self','theme','_links'])
-    num_forms = formList['items']
-    for forms in num_forms:
-        df1 = pd.DataFrame.from_dict(forms)
-        df = df.append(df1)
+    df = pd.DataFrame(columns=['id','title'])
+    for pesq in formList['items']:
+        df = df.append({'id': pesq['id'],'title': pesq['title']}
+        ,ignore_index=True)
     return df
 
 def tidyquestions(formQuestions: dict):
-    perguntas = formQuestions['fields']
-    x = 0
-    for pergunta in perguntas:
-        if x == 0:
-            df = pd.DataFrame.from_dict(pergunta)
+    df = pd.DataFrame(columns=['id_form'
+                                ,'title_form'
+                                ,'hidden_form'
+                                ,'welcome_screens'
+                                ,'thankyou_screens'
+                                ,'id_question'
+                                ,'title_question'
+                                ,'type_question'])
+    id_form = formQuestions['id']
+    title_form = formQuestions['title']
+    if 'hidden' in formQuestions:
+        hidden_form = formQuestions['hidden']
+    else:
+        hidden_form = ''
+    if 'welcome_screens' in formQuestions:
+        welcome_screens = formQuestions['welcome_screens'][0]['title']
+    else:
+        welcome_screens = ''
+    if 'thankyou_screens' in formQuestions:
+        thankyou_screens = formQuestions['thankyou_screens'][0]['title']
+    else:
+        thankyou_screens = ''
+    if 'fields' in formQuestions: 
+        quest = formQuestions['fields']
+        for i in range(len(quest)):
+            q = quest[i]
+            id_question = q['id'] 
+            title_question = q['title']
+            type_question = q['type']
+            df = df.append({'id_form': id_form
+                            ,'title_form': title_form
+                            ,'hidden_form': hidden_form
+                            ,'welcome_screens': welcome_screens
+                            ,'thankyou_screens': thankyou_screens 
+                            ,'id_question': id_question
+                            ,'title_question': title_question
+                            ,'type_question': type_question}
+                            ,ignore_index=True)
+    else:
+        df = df.append({'id_form': id_form
+                        ,'title_form': title_form
+                        ,'hidden_form': hidden_form
+                        ,'welcome_screens': welcome_screens
+                        ,'thankyou_screens': thankyou_screens 
+                        ,'id_question': ''
+                        ,'title_question': ''
+                        ,'type_question': ''}
+                        ,ignore_index=True)
+
+    return df
+
+
+def tidyAnswers(formAnswers: dict):
+    #create df to store answers
+    df = pd.DataFrame(columns=['landing_id'
+                            ,'landed_at'
+                            ,'submitted_at'
+                            ,'user_agent'
+                            ,'hidden'
+                            ,'calculated'
+                            ,'question_id'
+                            ,'question_type'
+                            ,'question_answer'])
+
+    landing_id = formAnswers['landing_id']
+    landed_at = formAnswers['landed_at']
+    submitted_at = formAnswers['submitted_at']
+    user_agent = formAnswers['metadata']['user_agent']
+    if 'hidden' in formAnswers:
+        hidden = formAnswers['hidden']
+    else:
+        hidden = ''
+    if 'calculated'in formAnswers:
+        calculated = formAnswers['calculated']
+    else:
+        calculated = ''
+
+    my_answers = formAnswers['answers']
+    for my_answer in my_answers:
+        question_id = my_answer['field']['id']
+        question_type = my_answer['type']
+        if question_type == 'choice':
+            question_answer = my_answer[question_type]
+            question_answer = question_answer['label']
+        elif question_type == 'choices':
+            question_answer = my_answer[question_type]
+            question_answer = question_answer['labels']
         else:
-            df1 = pd.DataFrame.from_dict(pergunta)
-            df = df.append(df1)
-    x = x + 1
-    return
+            question_answer = my_answer[question_type]
+        df = df.append({'landing_id': landing_id
+                        ,'landed_at': landed_at
+                        ,'submitted_at': submitted_at
+                        ,'user_agent': user_agent
+                        ,'hidden': hidden
+                        ,'calculated': calculated
+                        ,'question_id': question_id
+                        ,'question_type': question_type
+                        ,'question_answer': question_answer}
+                        ,ignore_index=True)
+    
+    return df
